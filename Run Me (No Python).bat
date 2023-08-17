fca20493b302a478e9bd35a390b14afaea5d0227
Bit Curious <125378535+AllwaysCurious@users.noreply.github.com>
2023-08-17 23:26:55 +0100

    Update Run Me (No Python).bat

--- Run Me (No Python).bat
+++ Run Me (No Python).bat
@@ -1,7 +1,10 @@
 @echo off
 setlocal
+set "voicemod_folder=C:\ProgramData\Voicemod"
 
-echo IMPORTANT! MAKE SURE THE PATH FILE ON THE LogFileCleaner.py PYTHON FILE IS SET TO YOUR VOICEMOD PROGRAMME DATA PATH!
+echo IMPORTANT! MAKE SURE THE PATH FILE ON THE Run Me (No Python).bat BATCH FILE IS SET TO YOUR VOICEMOD PROGRAMME DATA PATH!
+echo This Batch File is for users who don't have Python. We recommend the Python version, as it's more stable and customisable
+echo If you have the Python. Use the "Run Me.bat"
 echo This will clear all your log files in "C:\ProgramData\Voicemod" directory
 echo This has been tested for only Windows 10. This function should work with more recent versions of Windows. Hopefully!
 echo Would you like to proceed with the cleanup? (Y/N)
@@ -13,23 +16,29 @@ if errorlevel 2 (
     exit
 ) else (
     echo Okay then. Let's proceed!
-    echo One last thing! Would you like this script to repeat over and over again, until the programme has been closed? (Y/N)
+    echo One last thing! Would you like this batch to repeat over and over again, until the programme has been closed? (Y/N)
     choice /c yn /n
 
     if errorlevel 2 (
         cls
         echo Cleaning log files...
-        python LogFileCleaner.py
-
-        echo Exiting script. Thank you for using!
+        for %%F in ("%voicemod_folder%\*.log") do (
+            set "filename=%%~nF"
+            break > "%%F"
+            echo Emptied contents of file: !filename!.log
+        )
+        echo Exiting batch. Thank you for using!
         pause
         exit
     ) else (
         :loop
         cls
         echo Cleaning log files...
-        python LogFileCleaner.py
-        
+        for %%F in ("%voicemod_folder%\*.log") do (
+            set "filename=%%~nF"
+            break > "%%F"
+            echo Emptied contents of file: !filename!.log
+        )
         echo Looping again...
         echo Press Ctrl+C to stop the script.
         timeout /t 5 >nul
